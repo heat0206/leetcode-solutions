9dfeb2a7ac454270b0cc502658ec188a10b836e4
Heet <heetmankad06@gmail.com>
2025-08-30 10:15:14 +0530

    Added solution for maximum subarray sum

---
+++ Medium/53.Subarray_Sum.cpp
@@ -0,0 +1,30 @@
+//Leetcode Proble 53 : Subarray Sum
+//Difficulty: Medium
+//Approach : Used Kadane's Algorithm to optimize the solution.
+
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int maxSum=INT_MIN,currSum=0;
+        for(int st : nums)
+        {
+            //Calculate the sum by adding each number.
+            currSum += st;
+            if(currSum>=0)
+            {
+                maxSum = max(currSum,maxSum);
+            }
+            //If the sum is negative it wont lead to maximum subarray sum.
+            else
+            {
+                // If the vector is of only negative numbers
+                maxSum=max(currSum,maxSum);
+                currSum=0;
+            }
+        }
+        return maxSum;
+    }
+};
